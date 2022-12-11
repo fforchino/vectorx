fbbb63ecede34a8b016da776ba5be45a05844e3d
frcfppud <filippo.forchino@gmail.com>
2022-12-11 22:48:39 +0100

    WIP

--- pkg/intents/voicecommand-changelanguage.go
+++ pkg/intents/voicecommand-changelanguage.go
@@ -42,7 +42,7 @@ func changeLanguage(intent IntentDef, params IntentParams) string {
 			isSameLanguage = true
 		}
 		sdk_wrapper.SetLanguage(sdk_wrapper.LANGUAGE_ITALIAN)
-		sdk_wrapper.DisplayImage(sdk_wrapper.GetDataPath("languages/it.png"), 500, false)
+		sdk_wrapper.DisplayImage(sdk_wrapper.GetDataPath("images/languages/it.png"), 500, false)
 		break
 	case LOCALE_SPANISH:
 		sdk_wrapper.SetLocale("es-ES")
@@ -51,7 +51,7 @@ func changeLanguage(intent IntentDef, params IntentParams) string {
 			isSameLanguage = true
 		}
 		sdk_wrapper.SetLanguage(sdk_wrapper.LANGUAGE_SPANISH)
-		sdk_wrapper.DisplayImage(sdk_wrapper.GetDataPath("languages/es.png"), 500, false)
+		sdk_wrapper.DisplayImage(sdk_wrapper.GetDataPath("images/languages/es.png"), 500, false)
 		break
 	case LOCALE_FRENCH:
 		sdk_wrapper.SetLocale("fr-FR")
@@ -60,7 +60,7 @@ func changeLanguage(intent IntentDef, params IntentParams) string {
 			isSameLanguage = true
 		}
 		sdk_wrapper.SetLanguage(sdk_wrapper.LANGUAGE_FRENCH)
-		sdk_wrapper.DisplayImage(sdk_wrapper.GetDataPath("languages/fr.png"), 500, false)
+		sdk_wrapper.DisplayImage(sdk_wrapper.GetDataPath("images/languages/fr.png"), 500, false)
 		break
 	case LOCALE_GERMAN:
 		sdk_wrapper.SetLocale("de-DE")
@@ -69,7 +69,7 @@ func changeLanguage(intent IntentDef, params IntentParams) string {
 			isSameLanguage = true
 		}
 		sdk_wrapper.SetLanguage(sdk_wrapper.LANGUAGE_GERMAN)
-		sdk_wrapper.DisplayImage(sdk_wrapper.GetDataPath("languages/de.png"), 500, false)
+		sdk_wrapper.DisplayImage(sdk_wrapper.GetDataPath("images/languages/de.png"), 500, false)
 		break
 	default:
 		sdk_wrapper.SetLocale("en-US")
@@ -77,7 +77,7 @@ func changeLanguage(intent IntentDef, params IntentParams) string {
 			isSameLanguage = true
 		}
 		sdk_wrapper.SetLanguage(sdk_wrapper.LANGUAGE_ENGLISH)
-		sdk_wrapper.DisplayImage(sdk_wrapper.GetDataPath("languages/en.png"), 500, false)
+		sdk_wrapper.DisplayImage(sdk_wrapper.GetDataPath("images/languages/en.png"), 500, false)
 		break
 	}
 	sdk_wrapper.SayText(getText(STR_HELLO_WORLD))
