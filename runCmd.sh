1edd91c6bbe2dc8fda2b50c644e9e2ca2673cc27
Filippo Forchino <filippo.forchino@gmail.com>
2022-12-11 22:49:25 +0100

    wip

--- runCmd.sh
+++ runCmd.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+BASEDIR=$(dirname "$0")
+
+cd $BASEDIR
+
 if [[ ! -f ./source.sh ]]; then
   echo "You need to make a source.sh file. This can be done with the setup.sh script."
   exit 0
