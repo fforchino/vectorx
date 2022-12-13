718462db982d94a814d270a1f2c519826744f818
frcfppud <filippo.forchino@gmail.com>
2022-12-13 09:08:07 +0100

    WIP

--- source.sh
+++
@@ -1,6 +0,0 @@
-export WIREPOD_HOME=/home/pi/wire-pod
-export WIREPOD_EX_TMP_PATH=vectorfs/tmp
-export WIREPOD_EX_DATA_PATH=vectorfs/data
-export WIREPOD_EX_NVM_PATH=vectorfs/nvm
-export GOPATH=/usr/local/go
-export GOCACHE=/usr/local/go/pkg/mod
