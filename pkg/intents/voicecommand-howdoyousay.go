f621793c245e00dd332a8624dd7266a62ba31998
frcfppud <filippo.forchino@gmail.com>
2022-12-13 18:16:13 +0100

    Fix bug in howdoyousay

--- pkg/intents/voicecommand-howdoyousay.go
+++ pkg/intents/voicecommand-howdoyousay.go
@@ -26,7 +26,7 @@ func HowDoYouSay_Register(intentList *[]IntentDef) error {
 	*intentList = append(*intentList, intent)
 
 	addLocalizedString("STR_HOWDOYOUSAY_HOW_DO_YOU_SAY", []string{"how do you say ", "come si dice ", "como dicen ", "comme on dit ", "wie sie "})
-	addLocalizedString("STR_HOWDOYOUSAY_IN", []string{" in ", " a ", " en ", " en ", " auf "})
+	addLocalizedString("STR_HOWDOYOUSAY_IN", []string{" in ", " in ", " en ", " en ", " auf "})
 
 	return nil
 }
@@ -39,7 +39,7 @@ func HowDoYouSay(intent IntentDef, speechText string, params IntentParams) strin
 	wordSplitter := getText("STR_HOWDOYOUSAY_HOW_DO_YOU_SAY")
 	languageSplitter := getText("STR_HOWDOYOUSAY_IN")
 
-	println(speechText)
+	//println(speechText + "/" + wordSplitter + "/" + languageSplitter)
 	if strings.Contains(speechText, wordSplitter) && strings.Contains(speechText, languageSplitter) {
 		splitPhrase := strings.Split(speechText, wordSplitter)
 		tmp := strings.TrimSpace(splitPhrase[1])
@@ -47,24 +47,30 @@ func HowDoYouSay(intent IntentDef, speechText string, params IntentParams) strin
 		splitPhrase2 := strings.Split(tmp, languageSplitter)
 		word = splitPhrase2[0]
 		language = splitPhrase2[1]
-		println("Word: " + word + ", target language: " + language)
+		//println("Word: " + word + ", target language: " + language + "/" + getText(STR_LANGUAGE_ENGLISH))
 		trans := getText(STR_GENERIC_I_DONT_KNOW)
 		currentLanguage := sdk_wrapper.GetLanguage()
 		if word != "" {
 			if language == getText(STR_LANGUAGE_ENGLISH) {
+				sdk_wrapper.SetLocale("en-US")
 				sdk_wrapper.SetLanguage(sdk_wrapper.LANGUAGE_ENGLISH)
 				trans = sdk_wrapper.Translate(word, currentLanguage, sdk_wrapper.LANGUAGE_ENGLISH) + "!"
+				println(word + "/" + currentLanguage + "/" + sdk_wrapper.LANGUAGE_ENGLISH + "/" + trans)
 			} else if language == getText(STR_LANGUAGE_ITALIAN) {
+				sdk_wrapper.SetLocale("it-IT")
 				sdk_wrapper.SetLanguage(sdk_wrapper.LANGUAGE_ITALIAN)
 				trans = sdk_wrapper.Translate(word, currentLanguage, sdk_wrapper.LANGUAGE_ITALIAN) + "!"
 			} else if language == getText(STR_LANGUAGE_SPANISH) {
+				sdk_wrapper.SetLocale("es-ES")
 				sdk_wrapper.SetLanguage(sdk_wrapper.LANGUAGE_SPANISH)
 				trans = sdk_wrapper.Translate(word, currentLanguage, sdk_wrapper.LANGUAGE_SPANISH) + "!"
 			} else if language == getText(STR_LANGUAGE_FRENCH) {
 				sdk_wrapper.SetLanguage(sdk_wrapper.LANGUAGE_FRENCH)
+				sdk_wrapper.SetLocale("fr-FR")
 				trans = sdk_wrapper.Translate(word, currentLanguage, sdk_wrapper.LANGUAGE_FRENCH) + "!"
 			} else if language == getText(STR_LANGUAGE_GERMAN) {
 				sdk_wrapper.SetLanguage(sdk_wrapper.LANGUAGE_GERMAN)
+				sdk_wrapper.SetLocale("de-DE")
 				trans = sdk_wrapper.Translate(word, currentLanguage, sdk_wrapper.LANGUAGE_GERMAN) + "!"
 			} else {
 				returnIntent = STANDARD_INTENT_IMPERATIVE_NEGATIVE
@@ -72,6 +78,7 @@ func HowDoYouSay(intent IntentDef, speechText string, params IntentParams) strin
 		}
 		sdk_wrapper.SayText(trans)
 		sdk_wrapper.SetLanguage(currentLanguage)
+		sdk_wrapper.SetLocale("en-US")
 	}
 
 	return returnIntent
