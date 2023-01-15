30000d03e1d29224edcd0619ca27d4207998d269
frcfppud <filippo.forchino@gmail.com>
2023-01-15 22:18:52 +0100

    Revert to old stupid server

--- cmd/vimserver.go
+++ cmd/vimserver.go
@@ -5,9 +5,9 @@ import (
 	"encoding/json"
 	"fmt"
 	sdk_wrapper "github.com/fforchino/vector-go-sdk/pkg/sdk-wrapper"
-	"net/http"
 	"os"
 	"path/filepath"
+	"time"
 	"vectorx/pkg/intents"
 )
 
@@ -19,53 +19,45 @@ type botConfigStruct []struct {
 	IsEarlyOpus     bool   `json:"is_early_opus"`
 }
 
-func handlePost(w http.ResponseWriter, r *http.Request) {
-	messages, err := intents.VIMAPIParseIncomingMessages(r)
-	if err != nil {
-		http.Error(w, "Bad Request", http.StatusBadRequest)
-		return
-	}
-	defer r.Body.Close()
-
-	for i := 0; i < len(messages); i++ {
-		if !messages[i].Read {
-			println(fmt.Sprintf("[%d] New message from %s: %s", messages[i].Timestamp, messages[i].From, messages[i].Message))
-			var ctx = context.Background()
-			var start = make(chan bool)
-			var stop = make(chan bool)
-
-			sdk_wrapper.InitSDKForWirepod(messages[i].ToId)
-			go func() {
-				_ = sdk_wrapper.Robot.BehaviorControl(ctx, start, stop)
-			}()
-			done := false
-			for done == false {
-				select {
-				case <-start:
-					intents.VIMAPIPlayMessage(messages[i])
-					stop <- true
-					done = true
-				}
-			}
-			println("Message processed")
-		}
-	}
-}
-
 func main() {
 	// Just called to add VIM localized strings to the engine
 	intents.RegisterIntents()
 
-	/*
-		// Load the Vector Serial Numbers for which we are going to check messages from Wirepod
-		serials := getMyBotSerials()
-		for _, serial := range serials {
-			intents.VIMAPILogin("", serial)
-		}
-	*/
+	// Load the Vector Serial Numbers for which we are going to check messages from Wirepod
+	serials := getMyBotSerials()
 
-	http.HandleFunc("/", handlePost)
-	http.ListenAndServe(":8091", nil)
+	// Check for new messages forever
+	for {
+		for _, serial := range serials {
+			messages, err := intents.VIMAPICheckMessages(serial)
+			if err == nil && len(messages) > 0 {
+				for i := 0; i < len(messages); i++ {
+					if !messages[i].Read {
+						println(fmt.Sprintf("[%d] New message from %s: %s", messages[i].Timestamp, messages[i].From, messages[i].Message))
+						var ctx = context.Background()
+						var start = make(chan bool)
+						var stop = make(chan bool)
+
+						sdk_wrapper.InitSDKForWirepod(serial)
+						go func() {
+							_ = sdk_wrapper.Robot.BehaviorControl(ctx, start, stop)
+						}()
+						done := false
+						for done == false {
+							select {
+							case <-start:
+								intents.VIMAPIPlayMessage(messages[i])
+								stop <- true
+								done = true
+							}
+						}
+						println("Message processed")
+					}
+				}
+			}
+		}
+		time.Sleep(time.Duration(1000) * time.Millisecond)
+	}
 }
 
 func getMyBotSerials() []string {
--- pkg/intents/voicecommand-vim.go
+++ pkg/intents/voicecommand-vim.go
@@ -255,9 +255,7 @@ func sendMessageToChat(intent IntentDef, speechText string, params IntentParams)
 
 type VIMChatMessage struct {
 	From      string `json:"from"`
-	FromId    string `json:"from_id"`
-	To        string `json:"to"`
-	ToId      string `json:"to_id"`
+	FromId    int    `json:"from_id"`
 	Message   string `json:"message"`
 	Read      bool   `json:"read"`
 	Timestamp int    `json:"timestamp"`
@@ -428,16 +426,6 @@ func VIMAPICheckMessages(robotSerialNo string) ([]VIMChatMessage, error) {
 		return arr, err
 	}
 	return arr, errors.New("Unknown user")
-}
-
-func VIMAPIParseIncomingMessages(req *http.Request) ([]VIMChatMessage, error) {
-	var arr []VIMChatMessage
-	decoder := json.NewDecoder(req.Body)
-	err := decoder.Decode(&arr)
-	if err != nil && VIMDebug {
-		println(err.Error())
-	}
-	return arr, err
 }
 
 func VIMAPIPlayMessage(msg VIMChatMessage) {
