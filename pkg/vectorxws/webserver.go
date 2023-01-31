ef21ce90d0e78e887068a23b730240d3caa9cdf8
frcfppud <filippo.forchino@gmail.com>
2023-01-31 23:14:12 +0100

    Adding a webserver to VectorX

--- pkg/vectorxws/webserver.go
+++ pkg/vectorxws/webserver.go
@@ -14,7 +14,7 @@ import (
 
 func apiHandler(w http.ResponseWriter, r *http.Request) {
 	switch {
-	case r.URL.Path == "/api/get_kg_api":
+	case r.URL.Path == "/api/consistency_check":
 		mapConfig, err := wirepodConfigToJson()
 		if err != nil {
 			fmt.Fprintf(w, "{}")
