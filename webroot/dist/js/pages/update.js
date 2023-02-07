f14fd5bad19c82391fa4f8549a06eb381bdd1d6f
frcfppud <filippo.forchino@gmail.com>
2023-02-07 10:03:33 +0100

    New GUI for VectorX Web sever

--- webroot/dist/js/pages/update.js
+++ webroot/dist/js/pages/update.js
@@ -1,13 +1,11 @@
-function CheckUpdates() {
+async function CheckUpdates() {
     currentVersion = Settings.VECTORX_VERSION;
     document.getElementById("updates_update_status").innerHTML = "Current VectorX version is "+currentVersion+"<br/>";
     document.getElementById("updates_update_status").innerHTML += "Checking for updates, please wait...<br/>";
     RunUpdateScript().then((message) => {
         document.getElementById("updates_update_status").innerHTML += "<br/>Updates found and applied!";
         LoadSettings().then(() => {
-            if (currentVersion!=Settings.VECTORX_VERSION) {
-                document.getElementById("updates_update_status").innerHTML += "<br/>Current VectorX version is now "+Settings.VECTORX_VERSION;
-            }
+            document.getElementById("updates_update_status").innerHTML += "<br/>Current VectorX version is now "+Settings.VECTORX_VERSION;
         });
     });
 }
