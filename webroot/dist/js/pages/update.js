844b7eda8274be568f9d8b74d72968bb976f4a43
frcfppud <filippo.forchino@gmail.com>
2023-02-08 21:26:36 +0100

    Software update, final tests

--- webroot/dist/js/pages/update.js
+++ webroot/dist/js/pages/update.js
@@ -10,11 +10,11 @@ async function CheckUpdates() {
         LoadSettings().then(() => {
             if (Settings.VECTORX_VERSION == currentVersion) {
                 document.getElementById("update_result_no_update").style.display="block";
-                document.getElementById("updates_update_status").innerHTML += "<br/>No update found.";
+                document.getElementById("updates_update_status").innerHTML = "No update found.";
             }
             else {
                 document.getElementById("update_result_updated").style.display="block";
-                document.getElementById("updates_update_status").innerHTML += "<br/>Current VectorX version is now "+Settings.VECTORX_VERSION;
+                document.getElementById("updates_update_status").innerHTML = "New release installed!<br>Current VectorX version is now "+Settings.VECTORX_VERSION;
             }
         });
     });
@@ -40,6 +40,7 @@ async function RunUpdateScript() {
         }
 
     } else {
+        document.getElementById("updates_update_status").innerHTML = "Error. Updates cannot be applied automatically, you'll have to check what's going on using SSH.";
         document.getElementById("update_result_error").style.display="block";
     }
     document.getElementById("update_running").style.display = "none";
