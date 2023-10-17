d6dce9ba1224b62a929511ea3116042373c74361
frcfppud <filippo.forchino@gmail.com>
2023-10-17 15:39:14 +0200

    Checking

--- pkg/vectorxws/webserver.go
+++ pkg/vectorxws/webserver.go
@@ -21,7 +21,7 @@ import (
 	"vectorx/pkg/vim-server"
 )
 
-const VECTORX_VERSION = "RELEASE_21"
+const VECTORX_VERSION = "RELEASE_22"
 
 type WirePodConfig struct {
 	GlobalGuid string `json:"global_guid"`
